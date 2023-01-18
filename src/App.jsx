a8715ac90deec6f778809416549d2c2a5e5621ab
Josué Martell <josue.martell17@gmail.com>
2023-01-17 22:28:01 -0600

    feat: created signin page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { Routes, Route } from 'react-router-dom'
+import SignInPage from './pages/SignInPage'
+import TasksPage from './pages/TasksPage'
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={ <SignUpPage /> } />
+      <Route path="/" element={ <SignInPage /> } />
       <Route path="/tasks" element={ <TasksPage /> } /> 
     </Routes>) 
 } 
---
+++ src/pages/SignInPage.jsx
@@ -0,0 +1,11 @@
+import { Form } from "../components/UserForm/"
+
+function SignInPage() {
+	return (
+		<div className="min-h-screen w-screen grid place-items-center text-gray-800">
+			<Form />
+		</div>	
+	)
+}
+
+export default SignInPage
