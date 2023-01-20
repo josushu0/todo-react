a13428dc38b78cd7a5d8eb2cc67c5b49a53fead8
Josué Martell <josue.martell17@gmail.com>
2023-01-20 14:53:57 -0600

    style: changed some styles

--- src/components/UserForm/Form.jsx
+++ src/components/UserForm/Form.jsx
@@ -20,7 +20,7 @@ export function Form({signUp}) {
 			<Input type="text" name="name" label="Name" value={formState} setValue={setFormState}/>
 			<button
 				type="submit"
-				className="shadow w-full border-2 border-blue-500 px-3 py-2 mt-1 rounded-md transition-colors hover:bg-blue-500 hover:text-white focus:outline focus:outline-blue-500/50"
+				className="shadow w-full border-2 px-3 py-2 mt-1 rounded-md transition-colors bg-blue-500 text-white hover:bg-blue-600  focus:outline focus:outline-blue-500/50"
 			>
 				Sign In
 			</button>
