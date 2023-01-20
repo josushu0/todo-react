e4d2aebeee4372aa0c155ed6c0729eb676e875cf
Josué Martell <josue.martell17@gmail.com>
2023-01-19 23:04:44 -0600

    feat(redux): added create reducer

--- src/store/slices/jokes/jokesSlice.js
+++ src/store/slices/jokes/jokesSlice.js
@@ -1,15 +1,35 @@
 import { createSlice } from '@reduxjs/toolkit'
 
+const initialState = JSON.parse(localStorage.getItem('jokes')) ?? []
+
 export const jokesSlice = createSlice({
 	name: 'jokes',
-	initialState: {},
+	initialState,
 	reducers: {
-		create: (state) => {},
-		update: (state) => {},
-		remove: (state) => {}
+		create(state, action) {
+			const date = new Date()
+			const createdAt = `${date.getHours() + 1}:${date.getMinutes()} ${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`
+
+			state.push({
+				taskId: action.payload.id,
+				taskName: action.payload.setup,
+				taskContent: action.payload.punchline,
+				createdAt,
+				updatedAt: createdAt,
+				completed: false
+			})
+		},
+		update(state, action) {
+		},
+		remove(state, action) {
+		},
+		complete(state, action) {
+		},
+		clearAll(state) {
+		}
 	}
 })
 
-export const { create, update, remove } = jokesSlice.actions
+export const { create, update, remove, clearAll, complete } = jokesSlice.actions
 
 export default jokesSlice.reducer
