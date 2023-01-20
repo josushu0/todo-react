83b93b9dcd6173130b58a13c9bc00565f6fa44bf
Josué Martell <josue.martell17@gmail.com>
2023-01-20 14:53:37 -0600

    refactor: imported svg instead of getting it from public

--- src/components/TasksList/EmptyList.jsx
+++ src/components/TasksList/EmptyList.jsx
@@ -1,7 +1,9 @@
+import SadFace from "../../assets/SadFace.svg"
+
 function EmptyList() {
 	return (
 		<div className="w-full flex flex-col justify-center items-center gap-4 mt-10">
-			<img src="./SadFace.svg" className="h-20" />
+			<img src={ SadFace } className="h-20" />
 			<p className="">Get new Jokes by pressing the New button</p>
 		</div>
 	)
