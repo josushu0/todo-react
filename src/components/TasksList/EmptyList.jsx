16cfa88637924ca6aff4611af321a5b98eec4286
Josué Martell <josue.martell17@gmail.com>
2023-01-20 13:57:39 -0600

    feat(TasksPage): created a component to show when list is empty

---
+++ src/components/TasksList/EmptyList.jsx
@@ -0,0 +1,10 @@
+function EmptyList() {
+	return (
+		<div className="w-full flex flex-col justify-center items-center gap-4 mt-10">
+			<img src="./SadFace.svg" className="h-20" />
+			<p className="">Get new Jokes by pressing the New button</p>
+		</div>
+	)
+}
+
+export default EmptyList
